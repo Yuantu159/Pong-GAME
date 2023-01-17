e7ae506c4c782974120dca00a1172595d6d10bee
Yuantu159 <121108341+Yuantu159@users.noreply.github.com>
2023-01-17 18:34:11 +0800

    Add files via upload

--- PONG游戏.cpp
+++ PONG游戏.cpp
@@ -2,22 +2,39 @@
 #include<stdlib.h>
 #include<easyx.h>
 #include<conio.h>
-
+//_CRT_SECURE_NO_WARNINGS
 
 int main() {
-
+	int G = 0;
 	printf("P1：w=上，s=下\nP2:8=上，2=下\n");
+
+	//模式的选择
+	printf("选择游玩人数:	1.单人，2.双人\n");
+	while (1) {
+		scanf("%d", &G);
+		if (G != 1 && G != 2) {
+			printf("输入错误\n");
+			scanf("%d", &G);
+		}
+		else if (G==1) {
+			break;
+		}
+		else if (G==2) {
+			break;
+		}
+	}
 	system("pause");
 
 	//创建窗体
 	initgraph(800,600);
 	setorigin(400,300);
 	setaspectratio(1,-1);
+	setbkcolor(RGB(164, 225, 202));
 
 	//小球的初始化
 	int r = 10;		//圆的半径
-	int x = 0, y = 0;
-	int vx = -5, vy = 5;
+	int x = 0, y = 0;	
+	int vx = -5, vy = 5;	//圆的速度分量
 
 	//挡板的初始化
 	int x1 = -400, y1 = 150;		//挡板的左上角坐标
@@ -61,41 +78,93 @@ int main() {
 		solidrectangle(_2x1, _2y1, _2x2, _2y2);
 
 
-		//挡板的移动
-		if (_kbhit() != 0) {
-			char a1 = _getch();
-			switch (a1) {
-			case 'w':
-				if ( y1<= 300) {
-					y1 += 20;
-					y2 += 20;
+		
+		//双人模式挡板的移动
+		if (G==2) {
+			//挡板的移动
+			if (_kbhit() != 0) {
+				char a1 = _getch();
+				switch (a1) {
+				case 'w':
+					if (y1 <= 300) {
+						y1 += 20;
+						y2 += 20;
+					}
+					break;
+				case 's':
+					if (y2 >= -300) {
+						y1 -= 20;
+						y2 -= 20;
+					}
+					break;
 				}
-				break;
-			case 's':
-				if (y2 >= -300) {
-					y1 -= 20;
-					y2 -= 20;
+			}
+
+			//第二块挡板的移动
+			if (_kbhit() != 0) {
+				char a2 = _getch();
+				switch (a2) {
+				case '8':
+					if (_2y1 <= 300) {
+						_2y1 += 20;
+						_2y2 += 20;
+					}
+					break;
+				case'2':
+					if (_2y2 >= -300) {
+						_2y1 -= 20;
+						_2y2 -= 20;
+					}
+					break;
 				}
-				break;
-			case '8':
-				if (_2y1 <= 300) {
-					_2y1 += 20;
-					_2y2 += 20;
+			}
+		}
+
+		//单人模式
+		if (G==1) {
+
+			//挡板的移动
+			if (_kbhit() != 0) {
+				char a1 = _getch();
+				switch (a1) {
+				case 'w':
+					if (y1 <= 300) {
+						y1 += 20;
+						y2 += 20;
+					}
+					break;
+				case 's':
+					if (y2 >= -300) {
+						y1 -= 20;
+						y2 -= 20;
+					}
+					break;
 				}
-				break;
-			case'2':
-				if (_2y2 >= -300) {
-					_2y1 -= 20;
-					_2y2 -= 20;
+			}
+
+			//挡板的自动移动
+			if (x > 200) {
+				if (vy == 5) {	//挡板向上移动
+					if (_2y1 <= 300) {			//不让挡板超过地图
+						if (vx != -5) {								//弹走球后不会移动
+							_2y1 += 5;
+							_2y2 += 5;
+						}
+					}
 				}
-				break;
+				if (vy == -5) {	//挡板向下移动
+					if (_2y2 >= -300) {			//不让挡板超过地图
+						if (vx != -5) {								//弹走球后不会移动
+							_2y1 -= 5;
+							_2y2 -= 5;
+						}
+					}
+				}
 			}
 		}
 
 
-
-
-
+		//小球的失败判定
 		if (x >= 400 || x <= -400) {	//撞到两边
 			//小球的初始化
 			 r = 10;		//圆的半径
@@ -121,7 +190,7 @@ int main() {
 			 _2x2 = 400, _2y2 = -150;	//挡板的右下角坐标
 		}
 
-		Sleep(1);
+		Sleep(25);
 
 	}
 
